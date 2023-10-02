19463c9a835dac489417dc50a0a2e728a19f1705
Alexey <105539363+Hoobbaaboobba@users.noreply.github.com>
2023-10-03 00:27:43 +0300

    Add files via upload

--- components/ViewCatalog.tsx
+++ components/ViewCatalog.tsx
@@ -1,14 +1,7 @@
 "use client";
 
 import CatalogItem from "./Home/MainSection/CatalogItem";
-import {
-  blackMetal,
-  colorMetal,
-  metiza,
-  nershav,
-  precenSplav,
-  zincMetal,
-} from "./Home/MainSection/productsData";
+("./Home/MainSection/productsData");
 
 import OurContacts from "./Home/OurContacts/OurContacts";
 import useCatalogModal from "@/hooks/useCatalogModal";
@@ -17,73 +10,18 @@ import usePriceModal from "@/hooks/usePriceModal";
 import Link from "next/link";
 import useRequestModal from "@/hooks/useRequestModal";
 import { usePathname } from "next/navigation";
+import { metalArray } from "./Home/MainSection/data";
 
-const items = [
-  {
-    data: blackMetal,
-    label: "Чёрный металл",
-    img: "menuCatalog",
-    links: true,
-  },
-  {
-    data: colorMetal,
-    label: "Цветной метал",
-    img: "menuCatalogColor",
-    links: true,
-  },
-  {
-    data: nershav,
-    label: "Нержавеющая сталь",
-    img: "menuCatalogSteel",
-    links: true,
-  },
-  {
-    data: zincMetal,
-    label: "Оцинкованная сталь",
-    img: "menuCatalogZincSteel",
-    links: true,
-  },
-  {
-    data: metiza,
-    label: "Метизы",
-    img: "menuCatalogMetiza",
-    links: true,
-  },
-  {
-    data: precenSplav,
-    label: "Прецензионные сплавы",
-    img: "menuCatalogTruba",
-    links: true,
-  },
-  {
-    data: colorMetal,
-    label: "Позиции под заказы",
-    img: "menuCatalogStroy",
-    links: false,
-  },
-  {
-    data: colorMetal,
-    label: "Остатки",
-    img: "menuCatalogServices",
-    links: false,
-  },
-  {
-    data: colorMetal,
-    label: "Размотка арматура",
-    img: "menuCatalogStroy",
-    links: false,
-  },
-  {
-    data: colorMetal,
-    label: "Плазменная резка",
-    img: "menuCatalogPosotion",
-    links: false,
-  },
-];
+import Image from "next/image";
 
 const ViewCatalog = () => {
-  const { isOpenCatalog, onCloseCatalog, typeOfMenu, changeTypeOfMenu } =
-    useCatalogModal();
+  const {
+    isOpenCatalog,
+    onCloseCatalog,
+    typeOfMenu,
+    changeTypeOfMenu,
+    onCloseMenu,
+  } = useCatalogModal();
 
   const pathname = usePathname().split("/");
   const { onOpen } = usePriceModal();
@@ -102,6 +40,7 @@ const ViewCatalog = () => {
   const onClickLink = () => {
     changeTypeOfMenu("");
     onCloseCatalog();
+    onCloseMenu();
     document.body.style.overflowY = "auto";
   };
 
@@ -111,9 +50,9 @@ const ViewCatalog = () => {
         className={`fixed ${
           isOpenCatalog ? "translate-y-0" : "translate-y-[-120%]"
         } left-0 top-
-        0 h-[100dvh] w-full bg-white z-10 flex flex-wrap justify-center overflow-y-auto items-start p-5 pb-[160px] sm:px-8 transition duration-200 xl:hidden gap-1`}
+        0 h-[100dvh] w-full bg-white z-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 overflow-y-auto p-5 pb-[160px] sm:px-8 transition duration-200 xl:hidden gap-1`}
       >
-        {items.map((card, index) => (
+        {metalArray.map((card, index) => (
           <>
             <div key={index} onClick={() => changeTypeOfMenu(card.label)}>
               <CatalogItem src={card.img} label={card.label} bold="" />
@@ -125,62 +64,132 @@ const ViewCatalog = () => {
                   : "translate-y-[-120%]"
               } fixed z-50 p-4 pb-[150px] lg:pb-12 border w-full h-full overflow-x-hidden overflow-y-auto transition duration-200`}
             >
-              <div className="flex flex-col sm:flex-row gap-y-2 justify-center items-center sm:justify-between border-b border-b-black pb-4">
-                <h1 className="font-bold text-3xl uppercase">{card.label}</h1>
+              <div className="flex flex-col justify-center items-center text-center gap-2">
+                <h1 className="text-3xl font-bold uppercase">{card.label}</h1>
                 <div onClick={showPrice}>
-                  <OrangeButton label={"Уточнить размер или цены"} mark />
+                  <OrangeButton label={"Уточнить размер или цены"} />
                 </div>
               </div>
-              {card.links ? (
-                <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 pt-2 lg:p-4">
-                  {card.data.map((product) => (
-                    <div key={product.id} className="flex flex-col">
-                      <h2 className="font-bold text-xl">{product.label}</h2>
-                      <ul className="">
-                        {product.items.map((item: any, index: number) => (
-                          <Link
-                            key={index}
-                            href={`/${pathname[1] || "moscow"}/catalog/${product.label}/${item[0]}?i=${item[1]}&q=${item[2]}&amount=${item[3]}`}
-                          >
-                            <li
-                              onClick={onClickLink}
-                              className="opacity-60 first-letter:font-bold hover:opacity-100"
-                            >
-                              <h1>{item[0]}</h1>
-                            </li>
-                          </Link>
-                        ))}
-                      </ul>
-                    </div>
+              <hr className="w-full my-4" />
+              {card.items.length !== 0 ? (
+                <div
+                  className={`grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 `}
+                >
+                  {card.items.map((metal) => (
+                    <Link
+                      onClick={onClickLink}
+                      href={`/${pathname[1] || "moscow"}/catalog?q=${encodeURI(
+                        metal.name
+                      )}`}
+                      className="hover:shadow-md"
+                    >
+                      <Image
+                        src={require(`../public/${metal.img}.png`)}
+                        alt={card.label}
+                        width={240}
+                        height={280}
+                        placeholder="blur"
+                      />
+                    </Link>
                   ))}
                 </div>
               ) : (
-                <div className="w-full flex flex-col justify-center items-center">
-                  <div className="max-w-[600px] my-4">
-                    <h1>
-                      ООО &ldquo;КОМПРЕССМЕТАЛ&rdquo; предлагает к реализации
-                      невостребованные остатки металла от производства по
-                      оптовым ценам. Чтобы купить металл по сниженным ценам,
-                      ознакомьтесь с Прайсом на:
-                    </h1>
-                    <ul className="flex flex-col my-3">
-                      <li className="ml-4 relative before:content-[''] before:w-1 before:h-1 before:rounded-full before:bg-black before:absolute before:top-[12px] before:left-[-10px]">
-                        обрез металла
-                      </li>
-                      <li className="ml-4 relative before:content-[''] before:w-1 before:h-1 before:rounded-full before:bg-black before:absolute before:top-[12px] before:left-[-10px]">
-                        невостребованный металлопрокат
-                      </li>
-                      <li className="ml-4 relative before:content-[''] before:w-1 before:h-1 before:rounded-full before:bg-black before:absolute before:top-[12px] before:left-[-10px]">
-                        неликвиды металла
-                      </li>
-                    </ul>
-                    <h2>
-                      Возможен самовывоз или доставка.Цены ниже рыночных.По
-                      каждой позиции можно получить консультацию менеджера и
-                      оформить заказ в любой удобной вам форме:
-                    </h2>
-                  </div>
-                  <div onClick={showRequest}>
+                <div className="w-full p-4 flex flex-col gap-4">
+                  {card.label === "Резка металла в размер" ? (
+                    <div className="">
+                      <h1 className="text-xl">
+                        На нашем предприятии вы можете заказать резку
+                        металлопроката в размер, основными направлениями резки
+                        являются:
+                      </h1>
+                      <ul className="mt-4 ml-6 flex flex-col gap-4">
+                        <li className="dot">
+                          Резка труб в размер (позволяет получить отрезок трубы
+                          заданного размера для дальнейшего использования в
+                          производстве)
+                        </li>
+                        <li className="dot">
+                          Резка кругов в размер (наше оборудование позволяет
+                          резать круги до 420 мм)
+                        </li>
+                        <li className="dot">
+                          Резка проката в размер (в том числе для производства
+                          полосы)
+                        </li>
+                      </ul>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {card.label === "Покраска" ? (
+                    <div className="">
+                      <h1 className="text-xl">
+                        Опытные мастера утверждают, что успех покраски
+                        металлических изделий на 80% зависит от качества
+                        подготовки поверхности. Мы расскажем о главных тонкостях
+                        подготовки металла и сушки краски, чтобы результат
+                        радовал вас максимально долго.
+                      </h1>
+                      <ul className="mt-4 ml-6 flex flex-col gap-4">
+                        <li className="dot">Этап 1: Очищение поверхности</li>
+                        <li className="dot">Этап 2: Грунтование</li>
+                        <li className="dot">Этап 3: Покраска и сушка</li>
+                      </ul>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {card.label === "Размотка арматуры" ? (
+                    <div className="">
+                      <h1 className="text-xl">
+                        Размотка арматуры представляет собой технологический
+                        процесс, в ходе которого продукция в бухтах (катушках)
+                        распускается на прутки необходимой длины. Работы
+                        проводятся на специальных станах, рассчитанных на
+                        качественную обработку проката различного диаметра.
+                        Оборудование исключает повреждение и деформацию металла,
+                        в т. ч. скручивание по оси, влияющие на эксплуатационные
+                        характеристики, гарантируя оптимальные показатели
+                        геометрии. Оборудование для размотки арматуры состоят из
+                        таких узлов, как:
+                      </h1>
+                      <ul className="mt-4 ml-6 flex flex-col gap-4">
+                        <li className="dot">
+                          Блок управления, который позволяет задать необходимые
+                          параметры;
+                        </li>
+                        <li className="dot">Правильно-отрезной сектор;</li>
+                        <li className="dot">
+                          Место для приема и упаковывания прутков.
+                        </li>
+                      </ul>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {card.label === "Остатки" ? (
+                    <div className="">
+                      <h1 className="text-xl">
+                        ООО "КОМПРЕССМЕТАЛ" предлагает к реализации
+                        невостребованные остатки металла от производства по
+                        оптовым ценам. Чтобы купить металл по сниженным ценам,
+                        ознакомьтесь с Прайсом на:
+                      </h1>
+                      <ul className="mt-4 ml-6 flex flex-col gap-4">
+                        <li className="dot">Обрезь металла;</li>
+                        <li className="dot">Невостребованный металлопрокат;</li>
+                        <li className="dot">Неликвиды металла.</li>
+                      </ul>
+                      <p className="mt-3">
+                        Возможен самовывоз или доставка.Цены ниже рыночных.По
+                        каждой позиции можно получить консультацию менеджера и
+                        оформить заказ в любой удобной вам форме:
+                      </p>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  <div onClick={showRequest} className="mt-2">
                     <OrangeButton label={"Оставить заявку"} />
                   </div>
                   <OurContacts />
