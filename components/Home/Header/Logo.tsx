da38113da0a0f54b4166e8145e4ca43738f79456
Hoobbaboobba <xopoxopox@gmail.com>
2023-10-24 23:33:46 +0300

    commit

--- components/Home/Header/Logo.tsx
+++ components/Home/Header/Logo.tsx
@@ -5,13 +5,10 @@ import Link from "next/link";
 
 const Logo = () => {
   const { link } = useLocationModal();
-  const { onCloseCatalog, onCloseMenu } = useCatalogModal();
+  const { setCatalog } = useCatalogModal();
 
-  const closeMenu = () => {
-    onCloseCatalog(), onCloseMenu();
-  };
   return (
-    <Link href={`/${link}`} onClick={closeMenu}>
+    <Link href={`/${link}`} onClick={setCatalog}>
       <Image src="/logo.png" alt="logo" width={135} height={68} priority />
     </Link>
   );
--- hooks/useCatalogModal.ts
+++ hooks/useCatalogModal.ts
@@ -3,11 +3,13 @@ import { create } from "zustand";
 interface CatalogStore {
   isOpenCatalog: boolean;
   openCatalog: () => void;
+  setCatalog: () => void;
 }
 
 const useCatalogModal = create<CatalogStore>((set) => ({
   isOpenCatalog: false,
   openCatalog: () => set((state) => ({ isOpenCatalog: !state.isOpenCatalog })),
+  setCatalog: () => set({ isOpenCatalog: false }),
 }));
 
 export default useCatalogModal;
