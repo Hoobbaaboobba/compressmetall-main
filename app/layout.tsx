07e193a6e4dcd21cf6c1bab20954334107ee5c5b
Hoobbaboobba <xopoxopox@gmail.com>
2023-11-12 22:45:29 +0300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
           });`}
       </Script>
       <body>
-        <LoadingScreen />
+        {/* <LoadingScreen /> */}
         <LocationModal />
         <Request />
         <Call />
