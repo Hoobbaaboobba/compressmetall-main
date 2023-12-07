a8236dbad61a5ca36aef3874312dee500a2a75d8
Hoobbaboobba <xopoxopox@gmail.com>
2023-12-07 15:38:06 +0300

    commit

--- components/Home/MainSection/QueryLabel.tsx
+++ components/Home/MainSection/QueryLabel.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { notFound, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 const QueryLabel = () => {
   const searchParams = useSearchParams();
   const label = searchParams.get("label");
+
+  useEffect(() => {
+    if (!label) {
+      throw new Error("Страница не найдена");
+    }
+  });
+
   return <h1>{label}</h1>;
 };
 
