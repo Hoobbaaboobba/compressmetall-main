83c27e11b5a61946c81bb94205052f799819c59c
Hoobbaboobba <xopoxopox@gmail.com>
2024-01-14 21:43:48 +0300

    fix: loading screen

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -15,9 +15,11 @@ const LoadingScreen = () => {
     // }, 500);
     setLogo(true);
     setLoading(true);
-    setSeconds(100);
     setTimeout(() => {
-      setLoading(false);
+      setSeconds(100);
+    }, 300);
+    setTimeout(() => {
+      setLogo(false);
       setLoading(false);
     }, 1500);
 
@@ -27,21 +29,25 @@ const LoadingScreen = () => {
   return (
     <div
       className={`${
-        loading ? "opacity-100 z-[200]" : "opacity-0 hidden"
+        loading ? "opacity-100 z-[200]" : "opacity-0"
       } fixed flex-col gap-4 top-0 left-0 bg-white transition duration-300 w-full h-full px-8 flex justify-center items-center`}
     >
       <div className="h-[650px] mt-12 flex flex-col justify-center gap-4 items-center">
-        <Image
-          src="/loadingLogo.png"
-          alt="loadingLogo"
-          width={632}
-          height={545}
-          className={`${
-            logo ? "scale-100" : "scale-0"
-          } transition duration-300`}
-        />
+        <div className="w-[400px] h-[188px] lg:w-[828px] lg:h-[389px] relative">
+          <Image
+            src="/loadingLogo.png"
+            alt="loadingLogo"
+            fill
+            className={`${
+              logo ? "scale-100" : "scale-0"
+            } transition duration-300`}
+          />
+        </div>
         {loading && (
-          <Progress value={seconds} className="max-w-[400px] w-full" />
+          <Progress
+            value={seconds}
+            className="max-w-[300px] lg:max-w-[400px] w-full"
+          />
         )}
       </div>
     </div>
