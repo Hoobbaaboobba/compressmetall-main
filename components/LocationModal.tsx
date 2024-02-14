43fedd789c262facf11759ee8d212221844ba572
Alexey <105539363+Hoobbaaboobba@users.noreply.github.com>
2024-02-14 08:01:17 +0300

    fix: location modal

--- components/LocationModal.tsx
+++ components/LocationModal.tsx
@@ -160,11 +160,11 @@ const LocationModal = () => {
                   onClick={() => changeCityName(city.name, city.capital)}
                   key={city.capital}
                   className="w-full text-center text-xl hover:underline px-[15px] py-[5px]"
-                  href={
-                    currentUrl.replace(
+                  href={pathname ? 
+                    (currentUrl.replace(
                       `${pathname.split("/")[1]}`,
                       `${city.capital.split(" ").join("")}`
-                    ) || `${currentUrl}/${city.capital.split(" ").join("")}`
+                    )) : `/${city.capital.split(" ").join("")}`
                   }
                 >
                   <li>{city.name}</li>
