b191124b6aa6d64f83f44bf169bec864a72f3c1f
Alexey <rc5550463@gmail.com>
2024-09-29 22:03:24 +0300

    fix: ouradvantages

--- components/Home/advantages/Advantages.tsx
+++ components/Home/advantages/Advantages.tsx
@@ -9,19 +9,19 @@ const Advantages = () => {
                 <AdvantagesCard link="/services" big image="/service-icon.svg" topic="Наши услуги" description="Услуги для физических лиц и компаний в строительной индустрии" widthImg={60} heightImg={60}/>
             </div>
             <div className="w-full sm:h-[200px] flex flex-col sm:flex-row gap-4">
-                <AdvantagesCard link="/about_company/partners" image="/partners-icon.svg" topic="Партнеры" description="Услуги для физических лиц и компаний в строительной индустрии" widthImg={50} heightImg={50}/>
-                <AdvantagesCard link="/about_company/requisites"image="/details-icon.svg" topic="Реквизиты" description="Услуги для физических лиц и компаний в строительной индустрии" widthImg={50} heightImg={50}/>
+                <AdvantagesCard link="/about_company/partners" image="/partners-icon.svg" topic="Партнеры" description="Российские компании, с которыми мы сотрудничаем на постоянной основе" widthImg={50} heightImg={50}/>
+                <AdvantagesCard link="/about_company/requisites"image="/details-icon.svg" topic="Реквизиты" description="Ознакомьтесь с подробной информацией о нашей компании" widthImg={50} heightImg={50}/>
             </div>
         </div>
         <div className="w-full flex flex-col gap-4">
             <div className="w-full md:h-[200px] flex flex-col md:flex-row gap-4">
                 <AdvantagesCard price link="/services"image="/price-icon.svg" widthImg={50} topic="Прайс" description="Оставьте заявку, и наши менеджеры свяжутся с вами" heightImg={50}/>
-                <AdvantagesCard link="/directory/marki_stali"image="/book-icon.svg" topic="Справочник КМ" description="Услуги для физических лиц и компаний в строительной индустрии" widthImg={50} heightImg={50}/>
-                <AdvantagesCard link="/contacts"image="/contacts-icon.svg" topic="Наши контакты" description="Услуги для физических лиц и компаний в строительной индустрии" widthImg={50} heightImg={50}/>
+                <AdvantagesCard link="/directory/marki_stali"image="/book-icon.svg" topic="Справочник КМ" description="Марки стали с расшифровкой, заменители, аналоги, стандарты" widthImg={50} heightImg={50}/>
+                <AdvantagesCard link="/contacts"image="/contacts-icon.svg" topic="Наши контакты" description="Свяжитесь с нами любым удобным для вас способом" widthImg={50} heightImg={50}/>
             </div>
             <div className="w-full gap-4 flex flex-col sm:flex-row sm:h-[200px]">
-                <AdvantagesCard link="/about_company/vacancies"image="/work-icon.svg" topic="Трудоустройство" description="Услуги для физических лиц и компаний в строительной индустрии" widthImg={50} heightImg={50}/>
-                <AdvantagesCard link="/about_company"image="/company-icon.svg" topic="Наша компания" description="Услуги для физических лиц и компаний в строительной индустрии" widthImg={50} heightImg={50}/>
+                <AdvantagesCard link="/about_company/vacancies"image="/work-icon.svg" topic="Трудоустройство" description="Mы раcшиpяeм штaт и ждём активныx менеджеров, которые ставят цели и достигают их" widthImg={50} heightImg={50}/>
+                <AdvantagesCard link="/about_company"image="/company-icon.svg" topic="Наша компания" description="Узнайте больше о нас и о наших услугах" widthImg={50} heightImg={50}/>
             </div>
         </div>
       </div>
--- components/Home/advantages/AdvantagesCard.tsx
+++ components/Home/advantages/AdvantagesCard.tsx
@@ -26,11 +26,11 @@ const AdvantagesCard: React.FC<AdvantagesCardProps> = ({
 }) => {
   return (
     <Card className={`flex flex-col gap-2 relative ${big && "xl:items-end sm:justify-center"} cursor-pointer w-full h-full bg-light-gray p-6 border-none hover:shadow-md hover:bg-white hover:border transition duration-500`}>
-      <div className={`w-[60px] h-[60px] ${big && "md:absolute bottom-8 right-8"} `}>
+      <div className={`w-[60px] h-[60px] ${big && "md:absolute top-8 right-8"} `}>
         <Image src={image} alt="advantages" width={widthImg} height={heightImg} />
       </div>
       <CardContent className={!big ? "p-0" : "p-0 sm:p-auto"}>
-        <h3 className={`${big ? "text-2xl xl:text-3xl pr-2": "text-lg"} font-bold`}>{topic}</h3>
+        <h3 className={`${big ? "text-2xl xl:text-3xl": "text-lg"} font-bold`}>{topic}</h3>
         <p className={`opacity-80 leading-5 text-sm xl:text-md`}>{description}</p>
       </CardContent>
       <AdvantagesLink link={link} price={price || false}/>
